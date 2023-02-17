d8db5031235b397ab44613c3461f536fabc8090c
JoaoZenaro <47560029+JoaoZenaro@users.noreply.github.com>
2023-02-17 16:22:41 +0000

    ex2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # The C Programming Language
+
+Page: 27
+[The C Programming Language](https://kremlin.cc/k&r.pdf)
--- ch01/ex2.c
+++ ch01/ex2.c
@@ -4,11 +4,14 @@
 
 main()
 {
-    int fahr, cel;
+    float fahr, cel;
+
+    printf("Temperature Table\n");
+    printf("Fahrenheit\tCelcius\n");
 
     for (fahr = 0; fahr < 300; fahr += 20)
     {
-        cel = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, cel);
+        cel = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%8.0f\t%6.1f\n", fahr, cel);
     }
 }
